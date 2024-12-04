c001ce823b47d3a8bed4b65c957ff406d3952a42
sneha <sneha1423@protonmail.com>
2024-12-04 07:51:21 +0000

    preview component done

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
-function Preview({color,interior,wheel}) {
+import configs from "../app/assets/configs.json";
+console.log(configs);
+// 
+function Preview({
+  color = "Blue Planet",
+  interior = "white",
+  wheel = "Vortex",
+}) {
   return (
     <>
       <Image
-        src={"/images/preview.jpg"}
+        src={configs[color].wheels[wheel]}
         alt="preview"
         width="0"
         height="0"
@@ -11,11 +18,13 @@ function Preview({color,interior,wheel}) {
         className="w-full h-auto rounded-lg"
       />
       <div className="py-3">
-        <h3 className="font-bold text-xl text-blue-900">Metallic gloss</h3>
-        <p>A mysterious, seductive black with a metallic blue shimmer.</p>
+        <h3 className="font-bold text-xl text-blue-900">
+          {configs[color].finish}
+        </h3>
+        <p>{configs[color].description}</p>
       </div>
       <Image
-        src={"/images/interior.jpeg"}
+        src={`/images/interior/${interior}.jpeg`}
         alt="preview"
         width="0"
         height="0"
